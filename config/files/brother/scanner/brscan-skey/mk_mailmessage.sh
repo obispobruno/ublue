2ad132071cee525d0200151bfa21159f2f9499aa
obispobruno <obispobruno@tuta.com>
2024-05-05 15:14:18 -0300

    feat: add ulp

--- config/files/brother/scanner/brscan-skey/mk_mailmessage.sh
+++
@@ -1,6 +0,0 @@
-#!  /bin/bash
-if [ "$1" != 'r' ];then
-    echo "brscan-skey@$(uname -n)" > /etc//opt/brother/scanner/brscan-skey/brscan_mailmessage.txt
-else
-    rm /etc//opt/brother/scanner/brscan-skey/brscan_mailmessage.txt
-fi
